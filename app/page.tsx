ebd286e0fad71b03bc0698e9fe8dbd485b80c520
devvAyman <aymanechakar03@gmail.com>
2024-12-08 14:34:15 +0100

    Refactor components and styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,5 @@
-import AnimatedTooltip from "@/components/ui/animated-tooltip";
+import { Pricing } from "@/components/Pricing";
 
 export default function PricingPage() {
-  return (
-    <>
-      <div>This is the content</div>
-
-      <footer className="absolute bottom-3.5 mx-auto flex items-center gap-[0.5ch] left-1/2 -translate-x-1/2 text-center text-muted-foreground">
-        <span>Crafted by</span>
-        <AnimatedTooltip
-          label="Ayman Echakar"
-          className="font-semibold text-foreground transition-colors duration-300 ease-out"
-          icon={"/icons/ayman-favicon.svg"}
-          href="https://github.com/aymanch-03"
-        />
-      </footer>
-    </>
-  );
+  return <Pricing />;
 }
--- components/global/header.tsx
+++ components/global/header.tsx
@@ -1,24 +1,34 @@
 import { ThemeToggle } from "@/components/theme/theme-toggle";
 import { Button } from "@/ui/button";
 import { ArrowRight } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 export const Header = () => {
   return (
-    <header className="container mx-auto py-4 flex items-center justify-between h-fit">
-      <h1 className="text-xl font-bold">Pricing Component</h1>
+    <header className="container mx-auto flex h-fit items-center justify-between py-4">
+      <h1 className="text-xl font-bold flex items-center gap-2">
+        <Image
+          src={"/icons/shadcn-logo.png"}
+          width={30}
+          height={30}
+          className="rounded shadow-md border"
+          alt="shadcn/ui"
+        />
+        <span>Pricing Page</span>
+      </h1>
       <nav className="flex items-center gap-4">
         <ThemeToggle />
         <Link
           target="_blank"
-          href="https://github.com/aymanch-03/pricing-component"
+          href="https://github.com/aymanch-03/shadcn-pricing-page"
         >
           <Button
             variant={"expandIcon"}
             Icon={ArrowRight}
             iconPlacement="right"
-            className="h-fit rounded-full bg-secondary font-semibold text-foreground hover:bg-secondary/70"
+            className="h-fit rounded-full bg-[#222] font-semibold text-white hover:bg-[#222]/90"
           >
             Github
           </Button>
